31230a291071a1d0225c43bdbf0027af5e6e413d
Caroline <alyseflorez@hotmail.fr>
2022-07-11 15:49:03 +0200

    icon

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './App.css';
+
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepsMin = 0
+const stepsMax = 50000
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid" >
+        <div className="row">
+          <p>
+          {/* const Heart = 80
+          const Temperature = -20
+          const Steps = 0 */}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
